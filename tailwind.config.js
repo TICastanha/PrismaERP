ba9c488ae6183798168d0eadd71141ddf1d5c48f
lucas marques <lucascastanha24@gmail.com>
2025-08-31 12:48:59 -0300

    configurando padroes de estilizacao

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{html,js}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        prisma:{
+          primary: "#FF8C00",
+          secondary: "#2E2E2E",
+          third: "#FFF",
+          action: "#007BFF",
+          sucess: "#11b817ff",
+          alert: "#E53935",
+          backgr: "#F5F5F5"
+        }
+      },
+      fontFamily:{
+        inter: ["inter", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
