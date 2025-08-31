d23e616d3ca786a833dbf9376b388f935733222a
lucas marques <lucascastanha24@gmail.com>
2025-08-31 13:43:31 -0300

    configurando algumas coisas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent },
     {path:'**', redirectTo: 'login'}
 
