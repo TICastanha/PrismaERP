2a1972a1005203876ccb6b708120ec878bfc8b4b
lucas marques <lucascastanha24@gmail.com>
2025-09-01 12:24:51 -0300

    adicionando e configurando estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,11 @@ module.exports = {
         sm: "0 1px 2px rgba(0,0,0,0.05)",
         md: "0 4px 6px rgba(0,0,0,0.1)",
         lg: "0 10px 15px rgba(0,0,0,0.15)",
+      },
+      size: {
+        12: "3rem",
+        16: "4rem",
+        20: "40rem",
       }
     },
   },
